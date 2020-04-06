a72fa8eb87dcbbdb1f0298845f7d9e9a6e2564e0
Rascass <40122506+Rascass@users.noreply.github.com>
2020-04-06 15:36:47 +0300

    Update ExportSQL.sql

--- ExportSQL.sql
+++ ExportSQL.sql
@@ -1,14 +1,1 @@
-/*script generated for free with OnlineDataGenerator available at: https://www.onlinedatagenerator.com */
-CREATE TABLE MOCKDATA(ID BIGINT,JobTitle VARCHAR(500),EmailAddress VARCHAR(500),FirstNameLastName VARCHAR(500));
-
-INSERT INTO MOCKDATA VALUES('1','Designer','Leroy_Lee5499@guentu.biz','Leroy Lee');
-INSERT INTO MOCKDATA VALUES('2','Cook','Chelsea_King9313@twace.org','Chelsea King');
-INSERT INTO MOCKDATA VALUES('3','Inspector','Bob_Evans3461@brety.org','Bob Evans');
-INSERT INTO MOCKDATA VALUES('4','HR Coordinator','Tyler_King6706@acrit.org','Tyler King');
-INSERT INTO MOCKDATA VALUES('5','Cash Manager','Nick_Rust3811@hourpy.biz','Nick Rust');
-INSERT INTO MOCKDATA VALUES('6','Accountant','Rylee_Curtis322@twipet.com','Rylee Curtis');
-INSERT INTO MOCKDATA VALUES('7','Ambulatory Nurse','Luke_Isaac5711@irrepsy.com','Luke Isaac');
-INSERT INTO MOCKDATA VALUES('8','Treasurer','Hayden_Utterson3276@bulaffy.com','Hayden Utterson');
-INSERT INTO MOCKDATA VALUES('9','Treasurer','Alan_Stewart7737@bulaffy.com','Alan Stewart');
-INSERT INTO MOCKDATA VALUES('10','Inspector','Rufus_Warren3368@jiman.org','Rufus Warren');
 
